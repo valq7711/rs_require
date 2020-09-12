10e201e647c9b47931a3abbc1af41847762f50c7
unknown <valq7711@gmail.com>
2020-09-13 01:43:58 +0300

    iii

--- test/foo.js
+++ test/foo.js
@@ -1,11 +1,4 @@
-function inc(a){
-    return a+1;
-}
-
-describe("some", function() {
-
-  it("test some", function() {
-    assert.equal(inc(1), 2);
-  });
-
+define([], function(){
+    var a =12;    
 });
+
