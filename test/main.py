5f410f97866e8fb272cfa3e4e8e2737212d0d3f6
unknown <valq7711@gmail.com>
2020-09-13 03:11:08 +0300

    ii

--- test/main.py
+++ test/main.py
@@ -12,23 +12,19 @@ def err():
     shutdown()
     
 def shutdown():
-    #server.srv.server_close() 
-    #server.srv.shutdown()
-    sys.exit('error')
+    server.srv.server_close() 
+    server.srv.shutdown()
+    
 
 server = WSGIRefServer(port=8000, host='127.0.0.1')
 
 def run_srv():
-    try:
-        run(server=server)
-    except Exception:    
-        print('shutdown')
-        server.srv.server_close() 
-        server.srv.shutdown()
+    run(server=server)
+    print('shutdown')
 
 t = Thread(target=run_srv)
 t.daemon = True
 t.start()
 t.join()
-
+sys.exit('error')
 
