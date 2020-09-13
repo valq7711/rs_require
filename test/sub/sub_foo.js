2c5746d52a4bf367161308ce3bc76babdfa06b16
unknown <valq7711@gmail.com>
2020-09-14 01:55:04 +0300

    trigger build

--- test/sub/sub_foo.js
+++ test/sub/sub_foo.js
@@ -6,12 +6,13 @@ define(['foo', './sub_bar', 'there/foo'], function(foo, sub_bar, there_foo ){
     return ()=> 'sub/sub_foo';   
 });
 */
+
 define({'foo':'', './sub_bar':'', 'there/foo':'there_foo'}, function(pack ){
+    console.log('')
     log('sub/sub_foo <- foo:' + pack.foo());
     log('sub/sub_foo <- sub_bar:' + pack.sub_bar());
     log('sub/sub_foo <- there_foo:' + pack.there_foo());
     return ()=> 'sub/sub_foo';   
-
 });
 
 
